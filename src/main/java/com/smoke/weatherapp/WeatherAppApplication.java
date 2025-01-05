f2b10ee6eac383884fa6e80876265b2b6304b593
Daniel Castro <dcastro91@pm.me>
2025-01-05 14:14:13 +0100

    Bug fix - Incomplete Swagger documentation after containerizing the app

--- src/main/java/com/smoke/weatherapp/WeatherAppApplication.java
+++ src/main/java/com/smoke/weatherapp/WeatherAppApplication.java
@@ -2,8 +2,10 @@ package com.smoke.weatherapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.smoke.weatherapp.repository")
 public class WeatherAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/smoke/weatherapp/repository/TemperatureDataRepository.java
+++ src/main/java/com/smoke/weatherapp/repository/TemperatureDataRepository.java
@@ -4,7 +4,6 @@ import com.smoke.weatherapp.model.TemperatureData;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TemperatureDataRepository extends MongoRepository<TemperatureData, String> {
 
