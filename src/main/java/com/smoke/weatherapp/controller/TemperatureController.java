655bea1182f0124f34fe08fec2e8276e38d5df84
Daniel Castro <dcastro91@pm.me>
2025-01-05 15:19:28 +0100

    Added javadoc and validation annotations to request parameters in TemperatureController

--- src/main/java/com/smoke/weatherapp/controller/TemperatureController.java
+++ src/main/java/com/smoke/weatherapp/controller/TemperatureController.java
@@ -3,15 +3,21 @@ package com.smoke.weatherapp.controller;
 import com.smoke.weatherapp.model.TemperatureData;
 import com.smoke.weatherapp.model.TemperatureResponse;
 import com.smoke.weatherapp.service.TemperatureService;
+import com.smoke.weatherapp.util.Constants;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller for handling temperature data requests.
+ */
 @RestController
 @Tag(name = "Temperature", description = "Endpoints for temperature data")
 public class TemperatureController {
@@ -19,20 +25,35 @@ public class TemperatureController {
     @Autowired
     private TemperatureService temperatureService;
 
+    /**
+     * Fetches the current temperature for the given coordinates.
+     * If the data is cached and it has not expired, it will be returned from the cache.
+     *
+     * @param latitude  Latitude of the location.
+     * @param longitude Longitude of the location.
+     * @return TemperatureResponse containing the temperature data.
+     */
     @GetMapping("/temperature")
     @Operation(summary = "Get temperature data", description = "Fetches the current temperature for the given coordinates. If the data is cached and it has not expired, it will be returned from the cache.")
     public TemperatureResponse getTemperature(
-            @Parameter(description = "Latitude of the location", required = true) @RequestParam double latitude,
-            @Parameter(description = "Longitude of the location", required = true) @RequestParam double longitude) {
+            @Parameter(description = "Latitude of the location", required = true) @RequestParam @Min(Constants.MIN_LATITUDE_LONG) @Max(Constants.MAX_LATITUDE_LONG) double latitude,
+            @Parameter(description = "Longitude of the location", required = true) @RequestParam @Min(Constants.MIN_LATITUDE_LONG) @Max(Constants.MAX_LATITUDE_LONG) double longitude) {
         TemperatureData temperatureData = temperatureService.getTemperatureData(latitude, longitude);
         return new TemperatureResponse(temperatureData.getLatitude(), temperatureData.getLongitude(), temperatureData.getTemperature());
     }
 
+    /**
+     * Deletes the temperature data for the given coordinates from the database.
+     *
+     * @param latitude  Latitude of the location.
+     * @param longitude Longitude of the location.
+     * @return boolean indicating success or failure.
+     */
     @DeleteMapping("/temperature")
     @Operation(summary = "Delete temperature data", description = "Deletes the temperature data for the given coordinates from the database.")
     public boolean deleteTemperature(
-            @Parameter(description = "Latitude of the location", required = true) @RequestParam double latitude,
-            @Parameter(description = "Longitude of the location", required = true) @RequestParam double longitude) {
+            @Parameter(description = "Latitude of the location", required = true) @RequestParam @Min(Constants.MIN_LATITUDE_LONG) @Max(Constants.MAX_LATITUDE_LONG) double latitude,
+            @Parameter(description = "Longitude of the location", required = true) @RequestParam @Min(Constants.MIN_LATITUDE_LONG) @Max(Constants.MAX_LATITUDE_LONG) double longitude) {
         return temperatureService.deleteTemperatureData(latitude, longitude);
     }
 }
--- src/main/java/com/smoke/weatherapp/util/Constants.java
+++ src/main/java/com/smoke/weatherapp/util/Constants.java
@@ -7,6 +7,11 @@ public class Constants {
     public static final double MAX_LONGITUDE = 180.0;
     public static final double MIN_LONGITUDE = -180.0;
 
+    public static final long MIN_LATITUDE_LONG = (long) MIN_LATITUDE;
+    public static final long MAX_LATITUDE_LONG = (long) MAX_LATITUDE;
+    public static final long MIN_LONGITUDE_LONG = (long) MIN_LONGITUDE;
+    public static final long MAX_LONGITUDE_LONG = (long) MAX_LONGITUDE;
+
     public static final String ERROR_INVALID_COORDINATES = "Invalid coordinates";
     public static final String ERROR_OPEN_METEO_API_FAILURE = "Failed to get temperature data from Open-Meteo API";
 }
