2b72d7b6261921f69611c9bb88e5432e571197a6
Daniel Castro <dcastro91@pm.me>
2025-01-05 13:25:29 +0100

    Added Swagger configuration and endpoint documentation

--- src/main/java/com/smoke/weatherapp/controller/TemperatureController.java
+++ src/main/java/com/smoke/weatherapp/controller/TemperatureController.java
@@ -3,19 +3,26 @@ package com.smoke.weatherapp.controller;
 import com.smoke.weatherapp.model.TemperatureData;
 import com.smoke.weatherapp.model.TemperatureResponse;
 import com.smoke.weatherapp.service.TemperatureService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Temperature", description = "Endpoints for temperature data")
 public class TemperatureController {
 
     @Autowired
     private TemperatureService temperatureService;
 
     @GetMapping("/temperature")
-    public TemperatureResponse getTemperature(@RequestParam double latitude, @RequestParam double longitude) {
+    @Operation(summary = "Get temperature data", description = "Fetches the current temperature for the given coordinates. If the data is cached and it has not expired, it will be returned from the cache.")
+    public TemperatureResponse getTemperature(
+            @Parameter(description = "Latitude of the location", required = true) @RequestParam double latitude,
+            @Parameter(description = "Longitude of the location", required = true) @RequestParam double longitude) {
         TemperatureData temperatureData = temperatureService.getTemperatureData(latitude, longitude);
         return new TemperatureResponse(temperatureData.getLatitude(), temperatureData.getLongitude(), temperatureData.getTemperature());
     }
