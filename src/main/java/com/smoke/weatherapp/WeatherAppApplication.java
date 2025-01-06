5831a062aedcd81a7ef207a4ca3baf36f2a48f22
Daniel Castro <dcastro91@pm.me>
2025-01-06 10:08:52 +0100

    Added missing Validation configuration

--- src/main/java/com/smoke/weatherapp/WeatherAppApplication.java
+++ src/main/java/com/smoke/weatherapp/WeatherAppApplication.java
@@ -3,9 +3,11 @@ package com.smoke.weatherapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.validation.annotation.Validated;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.smoke.weatherapp.repository")
+@Validated
 public class WeatherAppApplication {
 
 	public static void main(String[] args) {
